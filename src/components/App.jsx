7dddfe598566fa3f1e070fa72422d8fc33bd84a1
AnriSanches <89216270+sanchesanri@users.noreply.github.com>
2024-01-15 13:45:22 +0200

    done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,120 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Statistic } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 import { Notification } from './Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-    1: 1,
+const NAME_BUTTON = ['good', 'neutral', 'bad'];
+
+export function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const changeStateValue = name => {
+    switch (name) {
+      case 'good':
+        setGood(prevValue => prevValue + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevValue => prevValue + 1);
+        break;
+      case 'bad':
+        setBad(prevValue => prevValue + 1);
+        break;
+
+      default:
+        break;
+    }
   };
 
-  changeStateValue = key => {
-    this.setState(prevState => ({
-      ...prevState,
-      [key]: prevState[key] + 1,
-    }));
+  const countTotalFeedback = () => {
+    return [good, neutral, bad].reduce((acc, item) => (acc += item), 0);
   };
 
-  countTotalFeedback = () => {
-    return Object.values(this.state).reduce((acc, item) => (acc += item), 0);
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-
-    return this.countTotalFeedback() !== 0
-      ? Math.ceil((Number(good) * 100) / Number(this.countTotalFeedback()))
+  const countPositiveFeedbackPercentage = () => {
+    return countTotalFeedback() !== 0
+      ? Math.ceil((Number(good) * 100) / Number(countTotalFeedback()))
       : 0;
   };
 
-  render() {
-    const { good, neutral, bad } = this.state;
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={this.state}
-            changeState={this.changeStateValue}
-          />
-        </Section>
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions options={NAME_BUTTON} changeState={changeStateValue} />
+      </Section>
 
-        <Section title="Statistic">
-          {this.countTotalFeedback() ? (
-            <ul>
-              <Statistic
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={this.countTotalFeedback}
-                positivePercentage={this.countPositiveFeedbackPercentage}
-              />
-            </ul>
-          ) : (
-            <Notification message="There is no feedback"></Notification>
-          )}
-        </Section>
-      </>
-    );
-  }
+      <Section title="Statistic">
+        {countTotalFeedback() ? (
+          <ul>
+            <Statistic
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback}
+              positivePercentage={countPositiveFeedbackPercentage}
+            />
+          </ul>
+        ) : (
+          <Notification message="There is no feedback"></Notification>
+        )}
+      </Section>
+    </>
+  );
 }
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
+
+//   changeStateValue = key => {
+//     this.setState(prevState => ({
+//       ...prevState,
+//       [key]: prevState[key] + 1,
+//     }));
+//   };
+
+//   countTotalFeedback = () => {
+//     return Object.values(this.state).reduce((acc, item) => (acc += item), 0);
+//   };
+
+//   countPositiveFeedbackPercentage = () => {
+//     const { good } = this.state;
+
+//     return this.countTotalFeedback() !== 0
+//       ? Math.ceil((Number(good) * 100) / Number(this.countTotalFeedback()))
+//       : 0;
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+//     return (
+//       <>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={this.state}
+//             changeState={this.changeStateValue}
+//           />
+//         </Section>
+
+//         <Section title="Statistic">
+//           {this.countTotalFeedback() ? (
+//             <ul>
+//               <Statistic
+//                 good={good}
+//                 neutral={neutral}
+//                 bad={bad}
+//                 total={this.countTotalFeedback}
+//                 positivePercentage={this.countPositiveFeedbackPercentage}
+//               />
+//             </ul>
+//           ) : (
+//             <Notification message="There is no feedback"></Notification>
+//           )}
+//         </Section>
+//       </>
+//     );
+//   }
+// }
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,29 +1,50 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import { BtnFeedBack } from './FeedBack.styled';
 
-export class FeedbackOptions extends Component {
+export function FeedbackOptions({ options, changeState }) {
+  const buttonsMarkup = (options, changeState) => {
+    return options.map(name => (
+      <BtnFeedBack
+        key={uuidv4()}
+        onClick={() => {
+          changeState(name);
+        }}
+      >
+        {name.charAt(0).toUpperCase() + name.slice(1)}
+      </BtnFeedBack>
+    ));
+  };
 
-    buttonsMarkup = (options, changeState) => {
-
-        return Object.keys(options).map(name => (
-
-            <BtnFeedBack
-                key={uuidv4()}
-                onClick={() => { changeState(name) }}>
-                {name.charAt(0).toUpperCase()
-                    + name.slice(1)}
-            </BtnFeedBack>
-        ))
-    };
-
-    render() {
-        const { options, changeState } = this.props;
-        return (
-            <>
-                <div>I'm feedback</div>
-                <div>{this.buttonsMarkup(options, changeState)}</div>
-            </>
-        );
-    }
+  return (
+    <>
+      <div>I'm feedback</div>
+      <div>{buttonsMarkup(options, changeState)}</div>
+    </>
+  );
 }
+
+// export class oldFeedbackOptions extends Component {
+//   buttonsMarkup = (options, changeState) => {
+//     return Object.keys(options).map(name => (
+//       <BtnFeedBack
+//         key={uuidv4()}
+//         onClick={() => {
+//           changeState(name);
+//         }}
+//       >
+//         {name.charAt(0).toUpperCase() + name.slice(1)}
+//       </BtnFeedBack>
+//     ));
+//   };
+
+//   render() {
+//     const { options, changeState } = this.props;
+//     return (
+//       <>
+//         <div>I'm feedback</div>
+//         <div>{this.buttonsMarkup(options, changeState)}</div>
+//       </>
+//     );
+//   }
+// }
